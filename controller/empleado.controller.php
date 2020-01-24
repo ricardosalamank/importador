ef481ea429d0ba8fb4844502034f2d614f22af79
rsalamanca <rsalamanca@solati.com.co>
2020-01-24 15:24:29 -0500

    ajuste de estandar

--- controller/empleado.controller.php
+++ controller/empleado.controller.php
@@ -1,38 +1,43 @@
 <?php
 require_once 'model/empleado.php';
 
-class EmpleadoController{
-    
+class EmpleadoController
+{
+
     private $model;
-    
-    public function __CONSTRUCT(){
+
+    public function __CONSTRUCT()
+    {
         $this->model = new Empleado();
     }
-    
-    public function Index(){
+
+    public function Index()
+    {
         require_once 'view/header.php';
         require_once 'view/empleado/empleado.php';
         require_once 'view/footer.php';
     }
-    
-    public function Crud(){
+
+    public function Crud()
+    {
         $alm = new Empleado();
-        
-        if(isset($_REQUEST['id'])){
+
+        if (isset($_REQUEST['id'])) {
             $alm = $this->model->Obtener($_REQUEST['id']);
         }
-        
+
         require_once 'view/header.php';
         require_once 'view/empleado/empleado-editar.php';
         require_once 'view/footer.php';
     }
 
-    public function forValid(){
+    public function forValid()
+    {
 
         $tipo = $_FILES['archivo']['type'];
- 
+
         $tamanio = $_FILES['archivo']['size'];
-        
+
         $archivotmp = $_FILES['archivo']['tmp_name'];
 
         $separador = $_POST['separador'];
@@ -44,85 +49,87 @@ class EmpleadoController{
 
         copy($archivotmp, 'archivos/copia.txt');
 
-        $labels = explode($separador,$lineas[0]);
+        $labels = explode($separador, $lineas[0]);
 
         $options = "<option value='N'>No Aplica</option>";
         foreach ($labels as $clave => $valor) {
-            $options .= "<option value='$clave'>$valor</option>";  
+            $options .= "<option value='$clave'>$valor</option>";
         }
 
 
         $estructura = $this->model->ObtenerEstructura($tabla);
-     
+
         $select = "";
         foreach ($estructura as $val) {
-            if($val->columna != 'codigo_campana' && $val->columna != 'consecutivo'){
+            if ($val->columna != 'codigo_campana' && $val->columna != 'consecutivo') {
                 $select .= "<label>$val->columna</label> Importar De ----->";
                 $select .= "<div class='form-group'><select class='form-control' name='$val->columna'>";
-                $select .=  $options;     
+                $select .= $options;
                 $select .= "</select></div><br>";
             }
-            
+
         }
-       // var_dump($select);
-        
+        // var_dump($select);
+
         /*
         if(isset($_REQUEST['id'])){
             $alm = $this->model->Obtener($_REQUEST['id']);
         */
-        
+
         require_once 'view/header.php';
         require_once 'view/empleado/empleado-editar.php';
         require_once 'view/footer.php';
     }
-    
-    public function Guardar(){
+
+    public function Guardar()
+    {
         $idCampana = $this->model->idCampana()->consecutivo;
         $lineas = file('archivos/copia.txt');
 
         $arrayEstruc = $_REQUEST;
         $arrayEstruc['codigo_campana'] = $idCampana;
         $arrayInsert = array();
-        
+
         foreach ($lineas as $clave => $valor) {
-            if($clave != 0){
-                $data = explode($arrayEstruc['separador'],$valor);
-                $campos="(";
+            if ($clave != 0) {
+                $data = explode($arrayEstruc['separador'], $valor);
+                $campos = "(";
                 $valores = "(";
-                foreach($arrayEstruc as $calve2 => $valor2){
-                    if($calve2!="c" && $calve2!="a" && $calve2!="separador" && $calve2!="tabla"){
-                        $campos .= $calve2.",";
-                        if($calve2=="codigo_campana"){
+                foreach ($arrayEstruc as $calve2 => $valor2) {
+                    if ($calve2 != "c" && $calve2 != "a" && $calve2 != "separador" && $calve2 != "tabla") {
+                        $campos .= $calve2 . ",";
+                        if ($calve2 == "codigo_campana") {
                             $valores .= "'$idCampana',";
                         } else {
-                            if($valor2 == 'N'){
+                            if ($valor2 == 'N') {
                                 $valores .= "null,";
                             } else {
-                                $valores .= "'".$data[$valor2]."',";
+                                $valores .= "'" . $data[$valor2] . "',";
                             }
                         }
                     }
                 }
                 $campos = substr($campos, 0, -1);
-                $campos.=")";
+                $campos .= ")";
                 $valores = substr($valores, 0, -1);
-                $valores.=")";
-                $arrayInsert[] = $campos." VALUES ".$valores;
+                $valores .= ")";
+                $arrayInsert[] = $campos . " VALUES " . $valores;
             }
         }
         $resp = 'S';
-         foreach($arrayInsert as $valor){
-             $result = $this->model->registrar($valor,$arrayEstruc['tabla']);
-             if(empty($result)){
-                 $resp = 'N';
-             } 
-          }
+        foreach ($arrayInsert as $valor) {
+            $result = $this->model->registrar($valor, $arrayEstruc['tabla']);
+            if (empty($result)) {
+                $resp = 'N';
+            }
+        }
 
         echo $resp;
-          
+
     }
-    
-    public function Eliminar(){
+
+    public function Eliminar()
+    {
         $this->model->Eliminar($_REQUEST['id']);
         header('Location: index.php');
     }
--- view/empleado/empleado-editar.php
+++ view/empleado/empleado-editar.php
@@ -3,55 +3,47 @@
 </h1>
 
 <ol class="breadcrumb">
-  <li><a href="?c=Empleado">Importar</a></li>
-  <li class="active">CAMPOS</li>
+    <li><a href="?c=Empleado">Importar</a></li>
+    <li class="active">CAMPOS</li>
 </ol>
 
 <form id="frm-import">
 
-<input type="hidden" name="separador" value="<?php echo $separador; ?>" />
-<input type="hidden" name="tabla" value="<?php echo $tabla; ?>" />
-   
+    <input type="hidden" name="separador" value="<?php echo $separador; ?>"/>
+    <input type="hidden" name="tabla" value="<?php echo $tabla; ?>"/>
+
     <?php echo $select; ?>
-    <hr />
+    <hr/>
 
 </form>
 
 
 <div class="text-right">
-        <button class="btn btn-success" onclick="importar()">Guardar</button>
-    </div>
-
-    
+    <button class="btn btn-success" onclick="importar()">Guardar</button>
+</div>
 
 
 <div id='resp'></div>
 <script>
-function importar(){
-        $.ajax({
-            type: "POST",
-            url: 'index.php?c=Empleado&a=Guardar',
-            data: $('#frm-import').serialize(),
-            success: function(response)
-            {
-                console.log(response);
-                if (response == "S"){
-                    $('#resp').html("<strong>Importado!</strong>");
-                    alert('Importado Exitosamente!');
-                } else {
-                    $('#resp').html("<strong>Fallo Importacion!</strong>");
-                    alert('Fallo Importacion!');
-                }
-              
-               
-           }
-       });
+  function importar () {
+    $.ajax({
+      type: 'POST',
+      url: 'index.php?c=Empleado&a=Guardar',
+      data: $('#frm-import').serialize(),
+      success: function (response) {
+        console.log(response)
+        if (response == 'S') {
+          $('#resp').html('<strong>Importado!</strong>')
+          alert('Importado Exitosamente!')
+        } else {
+          $('#resp').html('<strong>Fallo Importacion!</strong>')
+          alert('Fallo Importacion!')
+        }
 
-           
+      },
+    })
 
-}
+  }
 
 
-
-   
 </script>
